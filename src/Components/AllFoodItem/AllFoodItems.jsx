f9a730c4cd963ecef9017a469f8d7d9b7c32ffc1
AtiqurRahmanAtik <atikahmedtanvir84@gmail.com>
2024-11-06 22:16:27 +0600

    all foodItems data loading usnig tanstack query or react query

--- src/Components/AllFoodItem/AllFoodItems.jsx
+++ src/Components/AllFoodItem/AllFoodItems.jsx
@@ -1,25 +1,58 @@
 import { useEffect, useState } from "react";
 import FoodItemCard from "../FoodItemCard/FoodItemCard";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
 
 const AllFoodItems = () => {
 
-    const [foodItems, setFoodItems] = useState([]);
+    // const [foodItems, setFoodItems] = useState([]);
     // console.log(foodItems);
 
-    useEffect(()=>{
-        fetch('http://localhost:5000/foodItems')
-        .then(res=> res.json())
-        .then(data=> setFoodItems(data));
+    const fetchData = async () => {
+        const { data } = await axios.get('http://localhost:5000/foodItems');
+        return data;
+      };
 
-    },[]);
+    
+    const {data:foodItems, isLoading, error } = useQuery({
+        queryKey: ['foodItem'],
+        queryFn: fetchData,
 
 
 
+    })
+
+    // console.log(foodItems);
+
+
+    // data fetch using useEffect
+    // useEffect(()=>{
+    //     fetch('http://localhost:5000/foodItems',{
+    //         method: 'GET'
+    //     })
+    //     .then(res=> res.json())
+    //     .then(data=> setFoodItems(data));
+
+    // },[]);
+
+    if(isLoading){
+        return <>
+        <span className="loading loading-spinner text-primary"></span>
+<span className="loading loading-spinner text-secondary"></span>
+<span className="loading loading-spinner text-accent"></span>
+<span className="loading loading-spinner text-neutral"></span>
+<span className="loading loading-spinner text-info"></span>
+        </>
+    }
+
+    if(error){
+        return <><p> The Error is ${error}</p></>
+    }
     return (
         <div className=" grid gap-3 grid-cols-1 lg:grid-cols-3 my-14">
             {
-                foodItems.map(item=> <FoodItemCard key={item._id} item={item}></FoodItemCard>)
+                foodItems?.map(item=> <FoodItemCard key={item._id} item={item}></FoodItemCard>)
             }
         </div>
     );
--- src/main.jsx
+++ src/main.jsx
@@ -15,6 +15,13 @@ import LoginPage from './Components/Pages/LoginPage/LoginPage';
 import SingleItem from './Components/SingleFoodItem/SingleItem';
 import RegisterPage from './Components/Pages/RegisterPage/RegisterPage';
 import About from './Components/About/About';
+import {
+  QueryClient,
+  QueryClientProvider,
+  
+} from '@tanstack/react-query';
+
+const queryClient = new QueryClient()
 
 
 const router = createBrowserRouter([
@@ -62,6 +69,10 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+
+    <QueryClientProvider client={queryClient}>
+     <RouterProvider router={router} />
+    </QueryClientProvider>
+
   </StrictMode>,
 )
