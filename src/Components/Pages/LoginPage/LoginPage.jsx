1c5d10d11be805c6b6ee2be29de27c85169d9169
AtiqurRahmanAtik <atikahmedtanvir84@gmail.com>
2024-11-30 23:16:23 +0600

    added some text

--- src/Components/Pages/LoginPage/LoginPage.jsx
+++ src/Components/Pages/LoginPage/LoginPage.jsx
@@ -13,9 +13,11 @@ const LoginPage = () => {
   <div className="hero-content w-full flex-col ">
     <div className="text-center w-1/2 lg:text-left">
       <h1 className="text-5xl font-bold text-center my-3">Login now!</h1>
+
+      
+      <h1 className="text-5xl font-bold text-center my-3">Login now!</h1>
       
       
-     
     </div>
 
 
